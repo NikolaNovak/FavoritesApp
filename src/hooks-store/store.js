c0f8ccdaca843d3bee35b40dd6221606a0b1bbc1
Nikola Novak <nikola.novak.hr@gmail.com>
2020-08-07 18:21:35 +0200

    Added a products store & replaced react-context with the custom useStore hook

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -4,11 +4,11 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
   const setState = useState(globalState)[1];
 
-  const dispatch = (actionIdentifier) => {
-    const newState = actions[actionIdentifier](globalState);
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
     globalState = { ...globalState, ...newState };
 
     for (const listener of listeners) {
