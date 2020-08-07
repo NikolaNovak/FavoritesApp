124891bb0eaada22c003f60aa2aa48f3fb5047e2
Nikola Novak <nikola.novak.hr@gmail.com>
2020-08-07 18:39:38 +0200

    Removed console log

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -5,7 +5,6 @@ import { useStore } from "../../hooks-store/store";
 import "./ProductItem.css";
 
 const ProductItem = React.memo((props) => {
-  console.log("rendering");
   const dispatch = useStore(false)[1];
 
   const toggleFavHandler = () => {
